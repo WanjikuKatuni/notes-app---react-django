53f0d75de838d8820460e1ea7b4b7c67668858a2
wanjiku <annritawanjiku77@gmail.com>
2023-07-20 19:24:18 +0300

    added title, content and showed date

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+// format date displayed
+function getFormatDate(note){
+  return new Date(note.updated).toLocaleDateString()
+}
+// get title of note
+function getTitle(note){
+  let title = note.body.split(`\n`)[0]
+
+  if(title.length > 45){
+    return title.slice(0,45)
+  }
+  return title
+}
+// note content
+function getContent(note){
+  let title = getTitle(note)
+  let content = note.body.replaceAll('\n', ' ')
+  content = content.replaceAll(title, "")
+
+  if(content.length > 45){
+    return content.slice(0,45)
+  } else {
+    return content
+  }
+
+}
 export const ListItem = ({ note }) => {
   return (
     <div>
       <Link to={`/note/${note.id}`}>
         <div className="notes-list-item">
-            <h3>{note.body}</h3>
+            <h3>{getTitle(note)}</h3>
+            <p><span>{getFormatDate(note)}</span>{getContent(note)}</p>
         </div>
       </Link>
     </div>
